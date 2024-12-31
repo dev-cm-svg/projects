d6dfb8b7b15b2383e1d8c88cb13cd81b2659ae39
dev-cm-svg <test@test.com>
2024-12-30 21:58:38 -0500

    add

--- motto/pages/api/mottos.js
+++ motto/pages/api/mottos.js
@@ -1,5 +1,3 @@
 import handler from '../../server/api/mottos.js'; // Adjust the import path
-res.setHeader('Access-Control-Allow-Origin', process.env.API_URL); // Allow your frontend origin
-res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS'); // Allowed methods
 
 export default handler; // Export the handler
