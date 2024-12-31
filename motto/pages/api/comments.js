da02c342588d2f28b5ee80e4ec7891b9dac1ea7b
dev-cm-svg <test@test.com>
2024-12-30 21:53:00 -0500

    add header

--- motto/pages/api/comments.js
+++ motto/pages/api/comments.js
@@ -1,3 +1,5 @@
 import handler from '../../server/api/comments'; // Adjust the import path
+res.setHeader('Access-Control-Allow-Origin', process.env.API_URL); // Allow your frontend origin
+res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS'); // Allowed methods
 
 export default handler; // Export the handler
